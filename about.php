ecb986f3dde66ba4036e871ebc360b870aff1c47
ksudarsh <krishna.sudarshan@gmail.com>
2025-06-22 20:52:17 -0400

    Pretty print

--- about.php
+++ about.php
@@ -2,103 +2,118 @@
 <html lang="en">
 
 <head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="keywords" content="" />
-<meta name="description" content="" />
-<meta name="author" content="" />
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-<title>SRG Vedanta School</title>
-<link rel="shortcut icon" href="images/favicon.png" />
-<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
-<link rel="stylesheet" type="text/css" href="css/animate.css"/>
-<link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
-<link rel="stylesheet" type="text/css" href="css/font-awesome.css"/>
-<link rel="stylesheet" type="text/css" href="css/themify-icons.css"/>
-<link rel="stylesheet" type="text/css" href="css/flaticon.css"/>
-<link rel="stylesheet" type="text/css" href="revolution/css/rs6.css">
-<link rel="stylesheet" type="text/css" href="css/prettyPhoto.css">
-<link rel="stylesheet" type="text/css" href="css/shortcodes.css"/>
-<link rel="stylesheet" type="text/css" href="css/main.css"/>
-<link rel="stylesheet" type="text/css" href="css/responsive.css"/>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <title>SRG Vedanta School</title>
+    <link rel="shortcut icon" href="images/favicon.png" />
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/animate.css" />
+    <link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.css" />
+    <link rel="stylesheet" type="text/css" href="css/themify-icons.css" />
+    <link rel="stylesheet" type="text/css" href="css/flaticon.css" />
+    <link rel="stylesheet" type="text/css" href="revolution/css/rs6.css">
+    <link rel="stylesheet" type="text/css" href="css/prettyPhoto.css">
+    <link rel="stylesheet" type="text/css" href="css/shortcodes.css" />
+    <link rel="stylesheet" type="text/css" href="css/main.css" />
+    <link rel="stylesheet" type="text/css" href="css/responsive.css" />
 </head>
 
 <body>
 
-<!--HEADER STARTS-->
+    <!--HEADER STARTS-->
     <?php include 'header.php' ?>
-<!--HEADER ENDS-->
+    <!--HEADER ENDS-->
 
 
-<!-- INNER BG STARTS -->
-<div class="ttm-page-title-row">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12"> 
-                <div class="title-box text-left">
-                    <div class="page-title-heading">
-                        <h1>About Us</h1>
+    <!-- INNER BG STARTS -->
+    <div class="ttm-page-title-row">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="title-box text-left">
+                        <div class="page-title-heading">
+                            <h1>About Us</h1>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>                
-</div>
-<!-- INNER BG ENDS -->         
-
-<div class="container mt-4 mb-50">
-    <div class="row">
-
-        <div class="col-md-8 text-justify">
-<p>Sri Rangagopal school of Vedanta has been set up by the grace of the preceptors of Dr. Usha Narasimhan. After having studied Vedanta and passing the certification examinations in Vishishtadvaita, she was urged by her preceptor Dr. N. S. Anantharangachar, Vedanta Vidvan, to spread this authentic, traditional, philosophical knowledge so that the desirous deserving can benefit.</p>
-
-<p>In line with that, she began teaching the powerful Vedanta Desika stotras several decades ago. Many children and adults have benefitted from the same. Classes on prabhandas have been added.</p>
-
-<p>Her PhD thesis titled 'a working model of spiritual intelligence...' is a course blending Vedanta with modern management techniques. This has brought relief from stress to many people - especially the working middle-aged class. This is being offered to the general public and hopes to make inroads into corporates too.</p>
-
-<p>Vedanta texts learnt in the traditional kalakshepam mode are taught here in this school. People desiring to perfect their knowledge of Vishishtadvaita may attend the same after due consultation with the faculty.</p>
-
-<p>Senior scholarly faculty members are conducting classes in this school. All classes are offered in the offline and online mode. However, offline, direct, face to face interactions with the faculty is encouraged.</p>
-
-<p>Dr. Usha Narasimhan, a B. Tech from IIT Madras, 1986, has also done M.A. (Sanskrit) from KSOU, MPhil, Vidvat and PhD from Karnataka Sanskrit University.</p>
-
-<p>All services of the school are offered as a service to the divinity and are done only with the intention of pleasing the supreme almighty.</p>
     </div>
+    <!-- INNER BG ENDS -->
 
-        <div class="col-md-4">
-            <img src="images/about.jpg" class="img-fluid">
+    <div class="container mt-4 mb-50">
+        <div class="row">
+
+            <div class="col-md-8 text-justify">
+                <p>Sri Rangagopal school of Vedanta has been set up by the grace of the preceptors of Dr. Usha
+                    Narasimhan. After having studied Vedanta and passing the certification examinations in
+                    Vishishtadvaita, she was urged by her preceptor Dr. N. S. Anantharangachar, Vedanta Vidvan, to
+                    spread this authentic, traditional, philosophical knowledge so that the desirous deserving can
+                    benefit.</p>
+
+                <p>In line with that, she began teaching the powerful Vedanta Desika stotras several decades ago. Many
+                    children and adults have benefitted from the same. Classes on prabhandas have been added.</p>
+
+                <p>Her PhD thesis titled 'a working model of spiritual intelligence...' is a course blending Vedanta
+                    with modern management techniques. This has brought relief from stress to many people - especially
+                    the working middle-aged class. This is being offered to the general public and hopes to make inroads
+                    into corporates too.</p>
+
+                <p>Vedanta texts learnt in the traditional kalakshepam mode are taught here in this school. People
+                    desiring to perfect their knowledge of Vishishtadvaita may attend the same after due consultation
+                    with the faculty.</p>
+
+                <p>Senior scholarly faculty members are conducting classes in this school. All classes are offered in
+                    the offline and online mode. However, offline, direct, face to face interactions with the faculty is
+                    encouraged.</p>
+
+                <p>Dr. Usha Narasimhan, a B. Tech from IIT Madras, 1986, has also done M.A. (Sanskrit) from KSOU, MPhil,
+                    Vidvat and PhD from Karnataka Sanskrit University.</p>
+
+                <p>All services of the school are offered as a service to the divinity and are done only with the
+                    intention of pleasing the supreme almighty.</p>
+            </div>
+
+            <div class="col-md-4">
+                <img src="images/about.jpg" class="img-fluid">
+            </div>
+
         </div>
-
     </div>
-</div>
 
 
-<!--FOOTER STARTS-->
+    <!--FOOTER STARTS-->
     <?php include 'footer.php' ?>
-<!--FOOTER ENDS-->
+    <!--FOOTER ENDS-->
 
 
-<!--back-to-top start-->
-<a id="totop" href="#top">
-    <i class="fa fa-angle-up"></i>
-</a>
-<!--back-to-top end-->
+    <!--back-to-top start-->
+    <a id="totop" href="#top">
+        <i class="fa fa-angle-up"></i>
+    </a>
+    <!--back-to-top end-->
 
-<script src="js/jquery.min.js"></script>
-<script src="js/tether.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/jquery.easing.js"></script>    
-<script src="js/jquery-waypoints.js"></script>    
-<script src="js/jquery-validate.js"></script> 
-<script src="js/owl.carousel.js"></script>
-<script src="js/jquery.prettyPhoto.js"></script>
-<script src="js/numinate.min6959.js?ver=4.9.3"></script>
-<script src="js/lazysizes.min.js"></script>
-<script src="js/main.js"></script>
-<script src="revolution/js/revolution.tools.min.js"></script>
-<script src="revolution/js/rs6.min.js"></script>
-<script src="revolution/js/slider.js"></script>
-<!-- Javascript end-->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/tether.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.easing.js"></script>
+    <script src="js/jquery-waypoints.js"></script>
+    <script src="js/jquery-validate.js"></script>
+    <script src="js/owl.carousel.js"></script>
+    <script src="js/jquery.prettyPhoto.js"></script>
+    <script src="js/numinate.min6959.js?ver=4.9.3"></script>
+    <script src="js/lazysizes.min.js"></script>
+    <script src="js/main.js"></script>
+    <script src="revolution/js/revolution.tools.min.js"></script>
+    <script src="revolution/js/rs6.min.js"></script>
+    <script src="revolution/js/slider.js"></script>
+    <!-- Javascript end-->
 
 </body>
+
 </html>
